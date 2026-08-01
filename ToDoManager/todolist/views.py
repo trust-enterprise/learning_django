fd2fcd92141e40cd777be35a45732f4fe0a0a770
trust_enterprise <sheikhabdullah.aps@gmail.com>
2026-08-01 21:24:41 +0530

    added logo

--- ToDoManager/todolist/views.py
+++ ToDoManager/todolist/views.py
@@ -3,21 +3,33 @@ from django.shortcuts import render
 # from django.http import HttpResponse, JsonResponse
 
 def homepage(request):
-    return render(request, "main.html", {})
+    context = { 
+        'page': 'Homepage'
+    }
+    return render(request, "main.html", context)
 
 # Create your views here.
 def todolist(request):
     # data = {"name": "chanchal", "location": "mau"}
     # return HttpResponse("<h1>this is my response</h1>")
     # return JsonResponse(data)
-    return render(request, "todolist.html", {})
+    context = {
+            'page': 'Task List'
+        }
+    return render(request, "todolist.html", context)
 
 
 def contact(request):
-    return render(request, "contact.html", {})
+    context = {
+            'page': 'Contact'
+        }
+    return render(request, "contact.html", context)
 
 def about(request):
-    return render(request, "about.html", {})
+    context = {
+            'page': 'About'
+        }
+    return render(request, "about.html", context)
 
 
 
