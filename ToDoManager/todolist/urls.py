281e7c529533663680cee38eac51dce463eb5455
Sheikh Abdullah <sheikhabdullah.aps@gmail.com>
2026-08-01 05:42:39 +0530

    created html templates inside todolist app

--- ToDoManager/todolist/urls.py
+++ ToDoManager/todolist/urls.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-urlpatterns = [path("todolist/", views.todolist, name="todolist")]
+urlpatterns = [
+    path("", views.homepage, name="homepage"),
+    path("todolist/", views.todolist, name="todolist"),
+    path("contact/", views.contact, name="contact"),
+    path("about/", views.about, name="about"),
+               ]
--- ToDoManager/todolist/views.py
+++ ToDoManager/todolist/views.py
@@ -2,10 +2,23 @@ from django.shortcuts import render
 
 # from django.http import HttpResponse, JsonResponse
 
+def homepage(request):
+    return render(request, "main.html", {})
 
 # Create your views here.
 def todolist(request):
     # data = {"name": "chanchal", "location": "mau"}
     # return HttpResponse("<h1>this is my response</h1>")
     # return JsonResponse(data)
-    return render(request, "main.html", {})
+    return render(request, "todolist.html", {})
+
+
+def contact(request):
+    return render(request, "contact.html", {})
+
+def about(request):
+    return render(request, "about.html", {})
+
+
+
+
