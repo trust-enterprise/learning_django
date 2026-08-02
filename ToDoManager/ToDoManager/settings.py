c10b2768cc5197d966192cccb17194b9b098b304
trust_enterprise <sheikhabdullah.aps@gmail.com>
2026-08-02 07:08:42 +0530

    mysql configured with vs code

--- ToDoManager/ToDoManager/settings.py
+++ ToDoManager/ToDoManager/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "ToDoManager.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "tododb",
+        "USER": "root",
+        "PASSWORD": "Qwerty9%",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
--- ToDoManager/todolist/models.py
+++ ToDoManager/todolist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    # title = models.CharField(max_length=200)
+    # description = models.TextField(blank=True)
+    # completed = models.BooleanField(default=False)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # updated_at = models.DateTimeField(auto_now=True)
+    task = models.CharField(max_length=100)
+    is_completed = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.task}"
