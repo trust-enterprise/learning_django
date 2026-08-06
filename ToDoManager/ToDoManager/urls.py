374d05599f34c270a277d0ec38ddeb896c4a5d38
heksih <sheikh2isles@gmail.com>
2026-08-06 11:29:17 +0530

    user resgistration form created

--- ToDoManager/ToDoManager/urls.py
+++ ToDoManager/ToDoManager/urls.py
@@ -18,4 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("todolist.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), 
+               path("", include("todolist.urls")),
+               path("account/", include("users.urls")),
+               ]
---
+++ ToDoManager/users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from users.forms import CustomRegistrationForm
+from django.contrib import messages
+
+# Create your views here.
+def register(request):
+
+    if request.method == "POST":
+        register_form = CustomRegistrationForm(request.POST)
+        if register_form.is_valid():
+            register_form.save()
+            messages.success(request, "User creation successful")
+            return redirect("todolist")
+    else:
+        register_form = CustomRegistrationForm()
+    return render(request, "register.html", {'register_form': register_form})
